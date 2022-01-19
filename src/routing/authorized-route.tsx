7a6685c8de5e43d51399e780f254893814308405
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-01-19 19:15:41 +0100

    ✨ Auth route hoc

---
+++ src/routing/authorized-route.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps, FC, ReactChild, ReactNode } from 'react';
+import { Navigate, Route, RouteProps, useLocation } from 'react-router-dom';
+import { useAuthState } from '../hooks/use-auth-state/use-auth-state.hook';
+import { AppRoute } from './app-route.enum';
+
+interface Props {
+  children: JSX.Element;
+}
+
+export const RequireAuth = ({ children }: Props) => {
+  const location = useLocation();
+
+  const {
+    state: { isAuthorized },
+  } = useAuthState();
+
+  return isAuthorized ? (
+    children
+  ) : (
+    <Navigate
+      to={AppRoute.Login}
+      replace
+      state={{
+        path: location.pathname,
+      }}
+    />
+  );
+};
