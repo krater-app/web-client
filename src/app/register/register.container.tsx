e3e00553295a983f84f1bc9f3980efb7942bc2a3
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-02-14 19:45:40 +0100

    🌈 Refactor registration page to match new layout

--- src/app/register/register.container.tsx
+++ src/app/register/register.container.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { useMutation } from 'react-fetching-library';
 import { FieldValues } from 'react-hook-form';
 import { registerAction } from '../../api/actions/auth/auth.actions';
-import { NavbarLayout } from '../../theme/layouts/navbar.layout';
+import { HeroImgLayout } from '../../theme/layouts/hero-img.layout';
 import { Register } from './register';
 
 export const RegisterContainer = () => {
@@ -23,8 +23,8 @@ export const RegisterContainer = () => {
   );
 
   return (
-    <NavbarLayout>
+    <HeroImgLayout>
       <Register onSubmit={onSubmit} errorMessage={errorMessage} />
-    </NavbarLayout>
+    </HeroImgLayout>
   );
 };
--- src/app/register/register.tsx
+++ src/app/register/register.tsx
@@ -1,7 +1,7 @@
 import {
   Box,
   Button,
-  Flex,
+  Checkbox,
   FormControl,
   FormErrorMessage,
   FormHelperText,
@@ -10,7 +10,6 @@ import {
   Input,
   Link,
   Text,
-  VStack,
 } from '@chakra-ui/react';
 import { useCallback, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
@@ -54,133 +53,125 @@ export const Register = ({ onSubmit, errorMessage }: Props) => {
   );
 
   return (
-    <Box width="full">
-      <Flex
-        justify="center"
-        align={{
-          md: 'center',
-          sm: 'flex-start',
-        }}
+    <Box
+      width={{
+        base: 'container.sm',
+      }}
+    >
+      <Heading as="h2" fontSize="37px" mb={1}>
+        Register
+      </Heading>
+      <Text
+        mb={6}
+        fontWeight="medium"
+        color="font.black"
+        fontSize="19px"
+        maxW="70%"
       >
-        <Box
-          borderWidth={1}
-          borderRadius={8}
-          boxShadow="lg"
-          p={10}
-          w={{
-            md: 'container.md',
-            sm: 'container.sm',
-          }}
-          minW={{
-            sm: 'container.sm',
-          }}
+        An efficient way to be up to date with news from world of your interest.
+      </Text>
+      <form onSubmit={handleSubmit(handleSubmitCallback)} onReset={reset}>
+        {isError && (
+          <FormError title="Registration error" description={errorMessage} />
+        )}
+        <FormControl isInvalid={formState.errors.email}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            id="email"
+            placeholder="Provide your email"
+            type="email"
+            {...register('email', {
+              required: true,
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Invalid email address.',
+              },
+              minLength: 3,
+            })}
+            variant={formState.errors.email ? 'error' : 'outline'}
+          />
+          {formState.errors.email ? (
+            <FormErrorMessage>Invalid email address.</FormErrorMessage>
+          ) : (
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          )}
+        </FormControl>
+        <FormControl mt={6} isInvalid={formState.errors.nickname}>
+          <FormLabel htmlFor="nickname">Nickname</FormLabel>
+          <Input
+            id="nickname"
+            placeholder="Provider your nickname"
+            {...register('nickname', {
+              required: true,
+              minLength: 3,
+            })}
+          />
+          {formState.errors.nickname ? (
+            <FormErrorMessage>
+              Nickname is invalid, provide at least three characters.
+            </FormErrorMessage>
+          ) : (
+            <FormHelperText>
+              Nickname will be visible to other users. Also you will be able to
+              log in with username.
+            </FormHelperText>
+          )}
+        </FormControl>
+        <FormControl mt={6} isInvalid={formState.errors.password}>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            id="password"
+            placeholder="Provider your password"
+            type="password"
+            {...register('password', {
+              required: true,
+              minLength: 6,
+            })}
+          />
+          {formState.errors.password && (
+            <FormErrorMessage>
+              Invalid password, provide at least six characters.
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl mt={6} isInvalid={formState.errors.retypePassword}>
+          <FormLabel htmlFor="retype-password">Retype password</FormLabel>
+          <Input
+            id="retype-password"
+            placeholder="Retype your password"
+            type="password"
+            {...register('retypePassword', {
+              required: true,
+              minLength: 6,
+              validate: (value) => value === watch('password'),
+            })}
+          />
+          {formState.errors.retypePassword && (
+            <FormErrorMessage>The passwords do not match.</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl mt={6}>
+          <Checkbox size="lg">
+            I have read and agree with Terms & Services{' '}
+          </Checkbox>
+        </FormControl>
+        <Button
+          mt={6}
+          width="full"
+          type="submit"
+          colorScheme="orange"
+          isLoading={isRegistering}
+          loadingText="Registration in progress..."
         >
-          <VStack>
-            <Heading as="h2" size="lg" mb={4}>
-              Register
-            </Heading>
-          </VStack>
-          <form onSubmit={handleSubmit(handleSubmitCallback)} onReset={reset}>
-            {isError && (
-              <FormError
-                title="Registration error"
-                description={errorMessage}
-              />
-            )}
-            <FormControl isInvalid={formState.errors.email}>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input
-                id="email"
-                placeholder="Provide your email"
-                {...register('email', {
-                  required: true,
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: 'Invalid email address.',
-                  },
-                  minLength: 3,
-                })}
-                variant={formState.errors.email ? 'error' : 'outline'}
-              />
-              {formState.errors.email ? (
-                <FormErrorMessage>Invalid email address.</FormErrorMessage>
-              ) : (
-                <FormHelperText>We'll never share your email.</FormHelperText>
-              )}
-            </FormControl>
-            <FormControl mt={6} isInvalid={formState.errors.nickname}>
-              <FormLabel htmlFor="nickname">Nickname</FormLabel>
-              <Input
-                id="nickname"
-                placeholder="Provider your nickname"
-                {...register('nickname', {
-                  required: true,
-                  minLength: 3,
-                })}
-              />
-              {formState.errors.nickname ? (
-                <FormErrorMessage>
-                  Nickname is invalid, provide at least three characters.
-                </FormErrorMessage>
-              ) : (
-                <FormHelperText>
-                  Nickname will be visible to other users. Also you will be able
-                  to log in with username.
-                </FormHelperText>
-              )}
-            </FormControl>
-            <FormControl mt={6} isInvalid={formState.errors.password}>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input
-                id="password"
-                placeholder="Provider your password"
-                type="password"
-                {...register('password', {
-                  required: true,
-                  minLength: 6,
-                })}
-              />
-              {formState.errors.password && (
-                <FormErrorMessage>
-                  Invalid password, provide at least six characters.
-                </FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl mt={6} isInvalid={formState.errors.retypePassword}>
-              <FormLabel htmlFor="retype-password">Retype password</FormLabel>
-              <Input
-                id="retype-password"
-                placeholder="Retype your password"
-                type="password"
-                {...register('retypePassword', {
-                  required: true,
-                  minLength: 6,
-                  validate: (value) => value === watch('password'),
-                })}
-              />
-              {formState.errors.retypePassword && (
-                <FormErrorMessage>The passwords do not match.</FormErrorMessage>
-              )}
-            </FormControl>
-            <Button
-              mt={6}
-              width="full"
-              type="submit"
-              colorScheme="orange"
-              isLoading={isRegistering}
-              loadingText="Registration in progress..."
-            >
-              Register
-            </Button>
-          </form>
-          <Text mt={4}>
-            Already registered?
-            <Link ml={2} as={RouteLink} to={AppRoute.Login}>
-              Log In Here
-            </Link>
-          </Text>
-        </Box>
-      </Flex>
+          Register
+        </Button>
+      </form>
+      <Text mt={4}>
+        Already registered?
+        <Link ml={2} as={RouteLink} to={AppRoute.Login}>
+          Log In Here
+        </Link>
+      </Text>
     </Box>
   );
 };
